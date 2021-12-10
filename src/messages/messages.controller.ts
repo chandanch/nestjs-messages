ccfdcad2c4dff1d625a5897ed4861cf62316b77e
chandanch <chandan.cec107@gmail.com>
2021-12-10 08:55:51 +0530

    send error response :bulb:

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,39 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    NotFoundException,
+    Param,
+    Post,
+} from '@nestjs/common';
 import { CreateMessageDTO } from './dtos/create-message.dto';
+import { MessageService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    messageService: MessageService;
+    constructor() {
+        this.messageService = new MessageService();
+    }
+
     @Get()
     listMessages() {
-        return 'List messages';
+        return this.messageService.findAll();
     }
 
     @Post()
     createMessages(@Body() body: CreateMessageDTO) {
-        return { request: body };
+        return this.messageService.create(body.content);
     }
 
     @Get('/:id')
-    getMessage(@Param('id') id: string) {
-        return { message: `The message ID is ${id}` };
+    async getMessage(@Param('id') id: string) {
+        const message = await this.messageService.findOne(id);
+
+        if (!message) {
+            throw new NotFoundException(`message with ID: ${id} not found`);
+        }
+
+        return message;
     }
 }
--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -2,21 +2,21 @@ import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
     async findOne(id: string) {
-        const content = await readFile('messages.josn', { encoding: 'utf-8' });
+        const content = await readFile('messages.json', { encoding: 'utf-8' });
         const messages = JSON.parse(content);
 
         return messages[id];
     }
 
     async findAll() {
-        const content = await readFile('messages.josn', { encoding: 'utf-8' });
+        const content = await readFile('messages.json', { encoding: 'utf-8' });
         const messages = JSON.parse(content);
 
         return messages;
     }
 
     async create(message: string) {
-        const content = await readFile('messages.josn', { encoding: 'utf-8' });
+        const content = await readFile('messages.json', { encoding: 'utf-8' });
         const messages = JSON.parse(content);
 
         const id = Math.floor(Math.random() * 10000);
